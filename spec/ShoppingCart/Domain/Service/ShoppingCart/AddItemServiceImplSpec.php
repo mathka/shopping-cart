1a6aff59acd9c3e5697f4bdc8c5afd0027a55f37
Monika Czernicka <monika.czernicka@gmail.com>
2019-11-02 18:07:57 +0100

    largerThanAvailableInWarehouse in progress...

--- spec/ShoppingCart/Domain/Service/ShoppingCart/AddItemServiceImplSpec.php
+++ spec/ShoppingCart/Domain/Service/ShoppingCart/AddItemServiceImplSpec.php
@@ -7,12 +7,15 @@ use ShoppingCart\Domain\Exception\ShoppingCartException;
 use ShoppingCart\Domain\Model\ShoppingCart;
 use ShoppingCart\Domain\Model\Product;
 use ShoppingCart\Domain\Repository\ShoppingCartRepository;
+use ShoppingCart\Domain\Repository\WarehouseRepository;
 
 class AddItemServiceImplSpec extends ObjectBehavior
 {
-    public function let(ShoppingCartRepository $shoppingCartRepository)
-    {
-        $this->beConstructedWith($shoppingCartRepository);
+    public function let(
+        ShoppingCartRepository $shoppingCartRepository,
+        WarehouseRepository $warehouseRepository
+    ) {
+        $this->beConstructedWith($shoppingCartRepository, $warehouseRepository);
     }
 
     public function it_adds_item_to_shopping_cart(
@@ -46,6 +49,25 @@ class AddItemServiceImplSpec extends ObjectBehavior
         $this->shouldThrow(ShoppingCartException::lessThanMinimumOrderQuantity())
 
         // When
+            ->duringAdd($product, $quantity);
+    }
+
+    public function it_should_not_allow_item_to_be_added_when_quantity_is_larger_than_quantity_available_in_warehouse(
+        ShoppingCartRepository $shoppingCartRepository,
+        WarehouseRepository $warehouseRepository,
+        ShoppingCart $shoppingCart,
+        Product $product
+    ) {
+        //Given
+        $quantity = 0;
+        $product->hasLargerMinimumOrderQuantity($quantity)->willReturn(false);
+
+        $warehouseRepository->hasNotEnough($product, $quantity)->willReturn(false);
+
+        // Then
+        $this->shouldThrow(ShoppingCartException::largerThanAvailableInWarehouse())
+
+            // When
             ->duringAdd($product, $quantity);
     }
 }
--- src/ShoppingCart/Domain/Exception/ShoppingCartException.php
+++ src/ShoppingCart/Domain/Exception/ShoppingCartException.php
@@ -8,6 +8,11 @@ class ShoppingCartException extends \DomainException
 {
     public static function lessThanMinimumOrderQuantity(): self
     {
-        return new self('The quantity provided was less than the minimum required quantity of the product');
+        return new self('The quantity is less than the minimum required quantity of the product');
+    }
+
+    public static function largerThanAvailableInWarehouse(): self
+    {
+        return new self('The quantity of product is larger than currently available in the warehouse');
     }
 }
